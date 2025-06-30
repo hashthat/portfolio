3e34d3eea46cc3c38334e0a16d874a4adfed00bf
hashthat <samalberts73@gmail.com>
2025-06-30 07:25:55 -0500

    StaticScripts!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // For static site export (like GitHub Pages)
+  basePath: '/portfolio', // replace with your repo name if using project page
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Required for GitHub Pages + static export
+  },
+  trailingSlash: true, // Optional: adds trailing slashes for static routing
+};
 
 export default nextConfig;
+
