573c197e10f6c50139e3fde959c68dd875234cc0
hashthat <samalberts73@gmail.com>
2025-06-30 08:22:10 -0500

    addingBuilds

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export", // For static site export (like GitHub Pages)
   basePath: '/portfolio', // replace with your repo name if using project page
+  assetPrefix: '/portfolio/',  // must include trailing slash
   reactStrictMode: true,
   images: {
     unoptimized: true, // Required for GitHub Pages + static export
